e402fa279c762b35f5a71eb3590fa5eff587c22f
hardikvijay <146639732+hardikvijay@users.noreply.github.com>
2023-10-01 17:27:22 +0000

    variables and datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=2344
+let accountemail="hardik@gmail.com"
+var account_password="abc"
+accountcity="jaipur"
+
+accountemail="has@ga"
+account_password="12345"
+accountcity="bengaluru"
+let accountstate;
+
+
+// account_id="2"  // we can change it because we already declare it constant
+console.log(accountId)
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountemail,account_password,accountcity,accountstate])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3)  we are using nodejs , not browser
+
+console.log(3+3)
+console.log("hardik")
+
+let name="hardik"
+ 
+let age=18
+let isLoggedin=false //boolean
+
+//number => 2 to power 53
+//bigint
+//string=>" "
+// boolean=>true or false
+//null=>standalone vaue  //null is a datatype
+//undefined=>
+// symbol => unique
+//object
+
+console.log(typeof "hardik")   //string
+console.log(typeof "undefined") //undefined
+console.log(typeof "null") //object
