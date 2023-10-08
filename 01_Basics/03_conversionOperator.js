2e0f54569b4e58f53c7733591827898a649782b5
hardikvijay <146639732+hardikvijay@users.noreply.github.com>
2023-10-08 04:53:17 +0000

    conversionOPerator

---
+++ 01_Basics/03_conversionOperator.js
@@ -0,0 +1,70 @@
+/*//let score = "33"
+//let score = "33abc"           //"33abc"=> NaN
+
+//let score=null  //object    // null=>0
+// let score=undefined       //undefined=>NaN
+// let score=true           //ture=>1; false=>0
+let score="hardik"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)   // ise ye hoga ki ajo bhi number ya string hmne score ko di hogi vo isse no. bn jayega ye mainly us case me use hota hai jb hm backend pe work kr rhe honge tb hme pta nhi hoga ki fronted pe kisine isse score stringki form me diya hai number toh hum isse chage kr lenge number/string ki form me!
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+ */
+
+
+/*
+
+//let isLoggedin="true"
+// let isLoggedin=""
+let isLoggedin="hardik"
+let BooleanIsLoggedin=Boolean(isLoggedin)
+console.log(BooleanIsLoggedin)
+
+//1=>true ; 0=>false
+//""=>fasle ; "hardik"=>true
+*/
+
+
+/*
+let someNumber=33
+let Stringnumber=String(someNumber);
+console.log(Stringnumber)
+console.log(typeof Stringnumber)
+*/
+
+//**************************Operation*****************************//
+
+
+let value=3
+let negvalue=-value
+console.log(negvalue)
+
+console.log(2+2)
+console.log(22-2)
+console.log(2*2)
+console.log(s=2**3) //2^3
+console.log(2/3)
+console.log(2%3)//remainder
+
+let str1="hello"
+let str2=" hardik"
+let str3 =str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log((3+4)*5%3)
+
+console.log(+true)
+console.log(+"")
+
+let gamecounter=100;
+++gamecounter
+console.log(gamecounter);
+
+// read postfix and prefix from mdn js
